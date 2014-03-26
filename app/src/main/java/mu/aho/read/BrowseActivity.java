4fc4b4882743057d84ce04b193bf8d3f8a8711a5
ahomu <mail@ayumusato.com>
2014-03-26 20:49:46 +0900

    initial commit

---
+++ app/src/main/java/mu/aho/read/BrowseActivity.java
@@ -0,0 +1,7 @@
+package mu.aho.read;
+
+/**
+ * Created by ayumusato on 3/26/14.
+ */
+public class BrowseActivity {
+}
---
+++ app/src/main/java/mu/aho/read/EntriesFragment.java
@@ -0,0 +1,7 @@
+package mu.aho.read;
+
+/**
+ * Created by ahomu on 3/26/14.
+ */
+public class EntriesFragment {
+}
