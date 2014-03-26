caec328ae44c8a076fa2bf1b03e501d842b98972
ahomu <mail@ayumusato.com>
2014-03-26 21:46:53 +0900

    FragmentTabHostだとViewPagerとうまく連携できなかった

--- app/src/main/java/mu/aho/read/EntriesFragment.java
+++ app/src/main/java/mu/aho/read/EntriesFragment.java
@@ -1,7 +1,30 @@
 package mu.aho.read;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by ahomu on 3/26/14.
  */
-public class EntriesFragment {
+
+public class EntriesFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String name = getArguments().getString("name");
+        Log.d("TabFragment.onCreateView", name);
+
+        TextView textView = new TextView(getActivity());
+        textView.setGravity(Gravity.CENTER);
+        textView.setText(getArguments().getString("name"));
+
+        return textView;
+    }
 }
--- app/src/main/java/mu/aho/read/MainActivity.java
+++ app/src/main/java/mu/aho/read/MainActivity.java
@@ -16,7 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.TextView;
-
+import android.widget.TabHost.TabSpec;
 import java.util.ArrayList;
 import java.util.Locale;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
@@ -25,33 +25,48 @@ import android.support.v7.app.ActionBar.TabListener;
 
 // @see http://davidjkelley.net/?p=34
 // @see http://just-another-blog.net/programming/how-to-implement-horizontal-view-swiping-with-tabs/
+// @see http://k-1-ne-jp.blogspot.jp/2013/11/fragmenttabhost.html
+// @see http://yyyank.blogspot.jp/2013/07/androidapisummarize-deprecated-class.html
 /**
  * Created by ahomu on 3/26/14.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity implements FragmentTabHost.OnTabChangeListener {
 
     ViewPager mViewPager;
-    TabsAdapter mTabsAdapter;
-    private final static String TAG = "21st Polling:";
+    FragmentTabHost mTabHost;
+    private String[] TabTag = { "お気に入りお気に入りお気に入り", "tab2", "tab3", "tab4",
+            "tab5" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setDisplayShowHomeEnabled(false);
+//        final ActionBar actionBar = getSupportActionBar();
+//        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+//        actionBar.setDisplayShowTitleEnabled(false);
+//        actionBar.setDisplayShowHomeEnabled(false);
 
-        mViewPager = (ViewPager) findViewById(R.id.pager);
+        final FragmentTabHost mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+//        final ViewPager mViewPager = (ViewPager) findViewById(R.id.pager);
 
-        mTabsAdapter = new TabsAdapter(this, mViewPager);
-        mTabsAdapter.addTab(actionBar.newTab().setText(R.string.title_section1), PlaceholderFragment.class, null);
-        mTabsAdapter.addTab(actionBar.newTab().setText(R.string.title_section2), PlaceholderFragment.class, null);
-        mTabsAdapter.addTab(actionBar.newTab().setText(R.string.title_section3), PlaceholderFragment.class, null);
-        mTabsAdapter.addTab(actionBar.newTab().setText(R.string.title_section4), PlaceholderFragment.class, null);
-        mTabsAdapter.addTab(actionBar.newTab().setText(R.string.title_section5), PlaceholderFragment.class, null);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.content);
+
+        for (int i = 0; i < TabTag.length; i++) {
+
+            String name = TabTag[i];
+
+            Log.d("tabSpec", TabTag[i]);
+
+            Bundle bundle = new Bundle();
+            bundle.putString("name", name);
+
+            TabSpec tabSpec = mTabHost.newTabSpec(name);
+            tabSpec.setIndicator(name);
+            mTabHost.addTab(tabSpec, EntriesFragment.class, bundle);
+        }
+
+        mTabHost.setOnTabChangedListener(this);
 
 //        if (savedInstanceState == null) {
 //            getSupportFragmentManager().beginTransaction()
@@ -59,116 +74,15 @@ public class MainActivity extends ActionBarActivity {
 //                    .commit();
 //        }
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * 記事一覧のフラグメント（予定）
+    /*
+     * タブの選択が変わったときに呼び出される
+     *
+     * @see
+     * android.widget.TabHost.OnTabChangeListener#onTabChanged(java.lang.String)
      */
-    public static class PlaceholderFragment extends Fragment {
-
-        public static PlaceholderFragment newInstance(int sectionNum) {
-            PlaceholderFragment frag = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            frag.setArguments(args);
-            return frag;
-        }
-
-        public PlaceholderFragment() {}
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText("うぇーい");
-            return rootView;
-        }
-    }
-
-    public static class TabsAdapter extends FragmentPagerAdapter implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
-        private final Context mContext;
-        private final ActionBar mActionBar;
-        private final ViewPager mViewPager;
-        private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
-
-        static final class TabInfo {
-            private final Class<?> clss;
-            private final Bundle args;
-
-            TabInfo(Class<?> _class, Bundle _args) {
-                clss = _class;
-                args = _args;
-            }
-        }
-
-        public TabsAdapter(ActionBarActivity activity, ViewPager pager) {
-            super(activity.getSupportFragmentManager());
-            mContext = activity;
-            mActionBar = activity.getSupportActionBar();
-            mViewPager = pager;
-            mViewPager.setAdapter(this);
-            mViewPager.setOnPageChangeListener(this);
-        }
-
-        public void addTab(Tab tab, Class<?> clss, Bundle args) {
-            TabInfo info = new TabInfo(clss, args);
-            tab.setTag(info);
-            tab.setTabListener(this);
-            mTabs.add(info);
-            mActionBar.addTab(tab);
-            notifyDataSetChanged();
-        }
-
-        public int getCount() {
-            return mTabs.size();
-        }
-
-        public Fragment getItem(int position) {
-            TabInfo info = mTabs.get(position);
-            return Fragment.instantiate(mContext, info.clss.getName(), info.args);
-        }
-
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
-
-        public void onPageSelected(int position) {
-            mActionBar.setSelectedNavigationItem(position);
-        }
-
-        public void onPageScrollStateChanged(int state) {}
-
-        public void onTabSelected(Tab tab, FragmentTransaction ft) {
-            mViewPager.setCurrentItem(tab.getPosition());
-            Log.v(TAG, "clicked");
-            Object tag = tab.getTag();
-            for (int i=0; i<mTabs.size(); i++) {
-                if (mTabs.get(i) == tag) {
-                    mViewPager.setCurrentItem(i);
-                }
-            }
-        }
-
-        public void onTabUnselected(Tab tab, FragmentTransaction ft) {}
-
-        public void onTabReselected(Tab tab, FragmentTransaction ft) {}
+    @Override
+    public void onTabChanged(String arg0) {
+        // TODO Auto-generated method stub
+        Log.d("onTabChanged", "tabId:" + arg0);
     }
 }
