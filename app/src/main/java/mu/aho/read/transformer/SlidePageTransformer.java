78b8ed627ef6bfcfdfc1e2108963752270ed83e7
ahomu <mail@ayumusato.com>
2014-03-28 14:40:51 +0900

    PageTransformer壊してた(；´Д`)

--- app/src/main/java/mu/aho/read/transformer/SlidePageTransformer.java
+++ app/src/main/java/mu/aho/read/transformer/SlidePageTransformer.java
@@ -18,6 +18,14 @@ public class SlidePageTransformer implements PageTransformer {
         if (position >= 1 || position <= -1) {
             view.setTranslationX(0);
 
+        } else if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            view.setTranslationX(0);
+
+        } else if (position <= 0) { // [-1,0]
+            // Use the default slide transition when moving to the left page
+            view.setTranslationX(0);
+
         } else if (position <= 1) { // (0,1]
             // Counteract the default slide transition
             view.setTranslationX(pageWidth * -position);
@@ -25,7 +33,6 @@ public class SlidePageTransformer implements PageTransformer {
         } else { // (1,+Infinity]
             // This page is way off-screen to the right.
             view.setTranslationX(0);
-
         }
 
     }
